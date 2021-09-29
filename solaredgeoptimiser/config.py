bb2ffb025e2425b4f35d86bbd316ac1da3766118
Ed Rogers <etfrogers@hotmail.com>
2021-09-29 21:49:27 +0100

    Convert config to class to allow derived properties

--- solaredgeoptimiser/config.py
+++ solaredgeoptimiser/config.py
@@ -9,15 +9,47 @@ LOG_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 TIMESTAMP = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
 
-def to_times(str_list: List[str]) -> List[datetime.time]:
-    return [datetime.time.fromisoformat(t) for t in str_list]
+class Config:
+    def __init__(self, yaml_data: dict):
+        # for key, value in yaml_data.items():
+        #     setattr(self, self.key_to_attr(key), value)
+        #     print(f"self.{self.key_to_attr(key)} = yaml_data['{key}']")
+        self.solar_edge_api_key = yaml_data['solar-edge-api-key']
+        self.solar_edge_site_id = yaml_data['solar-edge-site-id']
+        self.solar_edge_account_id = yaml_data['solar-edge-account-id']
+        self.storage_profile_name = yaml_data['storage-profile-name']
+        self.site_location = yaml_data['site-location']
+        self.peak_time = yaml_data['peak-time']
+        self.overnight_usage = yaml_data['overnight-usage']
+        self.minimum_battery_level = yaml_data['minimum-battery-level']
+        self.reserve_capacity = yaml_data['reserve-capacity']
+
+        self.peak_time = self.to_times(self.peak_time)
+
+    def __getitem__(self, item):
+        return getattr(self, self.key_to_attr(item))
+
+    @property
+    def target_battery_level_after_discharge(self):
+        return self.minimum_battery_level + self.reserve_capacity + self.overnight_usage
+
+    @staticmethod
+    def to_times(str_list: List[str]) -> List[datetime.time]:
+        return [datetime.time.fromisoformat(t) for t in str_list]
+
+    @staticmethod
+    def key_to_attr(key: str) -> str:
+        return key.replace('-', '_')
+
+    @staticmethod
+    def attr_to_key(attr: str) -> str:
+        return attr.replace('_', '-')
 
 
-def load_config() -> dict:
+def load_config() -> Config:
     with open("site_config.yml", 'r') as file:
-        config = yaml.safe_load(file)
-    config['peak-time'] = to_times(config['peak-time'])
-    return config
+        config_data = yaml.safe_load(file)
+    return Config(config_data)
 
 
 def setup_logging():
