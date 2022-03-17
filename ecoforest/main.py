1e881706648ddc6eebae5d0de1f2640c9977c6f0
Ed Rogers <etfrogers@hotmail.com>
2022-03-17 07:54:07 +0000

    Switch to date class, and stop caching of today's data

--- ecoforest/main.py
+++ ecoforest/main.py
@@ -1,25 +1,26 @@
+import datetime
+
 import requests
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 class Dataset:
-    def __init__(self, day: int, month: int, year: int):
-        self.day = day
-        self.month = month
-        self.year = year
+    def __init__(self, date: datetime.date):
+        self.date = date
         use_cache = True
         if use_cache:
             try:
-                with open(f'data/{year}-{month:02d}-{day}.csv') as file:
+                with open(self._cache_file) as file:
                     contents = file.read()
             except FileNotFoundError:
-                contents = self.get_data_from_server(day, month, year)
-                # TODO do not cache today's data
-                with open(f'data/{year}-{month:02d}-{day}.csv', 'w') as file:
-                    file.write(contents)
+                contents = self.get_data_from_server()
+                if self.date != datetime.datetime.today().date():
+                    # do not cache today's data as it will change
+                    with open(self._cache_file, 'w') as file:
+                        file.write(contents)
         else:
-            contents = self.get_data_from_server(day, month, year)
+            contents = self.get_data_from_server()
         self._full_data = self.process_file_data(contents)
 
         # indices taken from Javascript plotting code
@@ -44,25 +45,36 @@ class Dataset:
     def daily_cop(self):
         return np.nanmean(self.cop)
 
-    def plot(self):
-        ax1 = plt.subplot(1, 1, 1)
+    def plot(self, axes=None):
+        if axes is None:
+            ax1 = plt.subplot(1, 1, 1)
+            ax2 = ax1.twinx()
+        else:
+            ax1, ax2 = axes
         ax1.plot(self.consumption, label=f'Electrical power: {self.total_consumption:.2f} kWh', color='red')
         ax1.plot(self.heating, label=f'Heating power: {self.total_heating:.2f} kWh', color='lightgreen')
         ax1.set_ylabel('kW')
-        ax2 = ax1.twinx()
         ax2.plot(self.cop, label=f'Mean COP: {self.daily_cop:.2f}')
         ax1.legend()
         ax2.legend(loc='upper left')
-        plt.suptitle(f'{self.day}/{self.month}/{self.year}')
+        plt.suptitle(f'{self.date_str}')
         plt.show()
+        return [ax1, ax2]
 
-    @staticmethod
-    def get_data_from_server(day, month, year):
+    @property
+    def date_str(self):
+        return self.date.strftime('%Y-%m-%d')
+
+    @property
+    def _cache_file(self):
+        return f'data/{self.date_str}.csv'
+
+    def get_data_from_server(self):
         server = '192.168.1.147'
         port = '8000'
         data_dir = 'historic'
         serial = '**REMOVED**'
-        filename = f'{year}-{month:02d}-{day}_{serial}_1_historico.csv'
+        filename = f'{self.date_str}_{serial}_1_historico.csv'
         url = f'https://{server}:{port}/{data_dir}/{filename}'
         key = '**REMOVED**'
         response = requests.get(url,
@@ -89,7 +101,7 @@ def main():
     month = 3
     # day = 10
     for day in range(10, 17):
-        data = Dataset(day, month, year)
+        data = Dataset(datetime.date(year, month, day))
         data.plot()
 
 
