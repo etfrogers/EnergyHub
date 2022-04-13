af87c2521516f3c20b4dad03124c4ed350b8f499
Ed Rogers <etfrogers@hotmail.com>
2022-04-13 18:08:59 +0100

    Extract Dataset class

--- ecoforest/main.py
+++ ecoforest/main.py
@@ -7,71 +7,40 @@ from matplotlib.dates import DateFormatter
 
 CSV_DATE_FORMAT = '%Y/%m/%d %H:%M:%S'
 EXPLORE_UNUSED_DATA = False
+TIME_FORMAT = DateFormatter("%H:%M")
 
 
 class Dataset:
-    def __init__(self, date: datetime.date):
-        self.date = date
-        use_cache = True
-        if use_cache:
-            try:
-                with open(self._cache_file) as file:
-                    contents = file.read()
-            except FileNotFoundError:
-                contents = self.get_data_from_server()
-                if self.date != datetime.datetime.today().date():
-                    # do not cache today's data as it will change
-                    with open(self._cache_file, 'w') as file:
-                        file.write(contents)
-        else:
-            contents = self.get_data_from_server()
-        self.timestamps, self._full_data = self.process_file_data(contents)
+    # indices taken from Javascript plotting code
+    # (need to subtract 2 from the indices used there to allow for serial and timestamp)
+    MAPPING = {24: 'consumption',
+               25: 'heating',
+               26: 'cooling',
+               10: 'dhw_temp',
+               # no index for 'pool'
+               13: 'heating_buffer_temp',
+               14: 'cooling_buffer_temp',
+               15: 'production_supply',
+               16: 'production_return',
+               17: 'brine_supply',
+               18: 'brine_return',
+               11: 'outdoor_temp',
+               19: 'zone_1',
+               20: 'zone_2',
+               21: 'zone_3',
+               22: 'zone_4',
+               }
 
+    def __init__(self, timestamps, data):
         self.heating = self.consumption = self.cooling = None
         self.dhw_temp = self.heating_buffer_temp = self.cooling_buffer_temp = None
         self.production_supply = self.production_return = self.brine_supply = self.brine_return = None
         self.outdoor_temp = None
 
-        # indices taken from Javascript plotting code
-        # (need to subtract 2 from the indices used there to allow for serial and timestamp)
-        mapping = {24: 'consumption',
-                   25: 'heating',
-                   26: 'cooling',
-                   10: 'dhw_temp',
-                   # no index for 'pool'
-                   13: 'heating_buffer_temp',
-                   14: 'cooling_buffer_temp',
-                   15: 'production_supply',
-                   16: 'production_return',
-                   17: 'brine_supply',
-                   18: 'brine_return',
-                   11: 'outdoor_temp',
-                   19: 'zone_1',
-                   20: 'zone_2',
-                   21: 'zone_3',
-                   22: 'zone_4',
-                   }
-        for index, name in mapping.items():
-            setattr(self, name, self._full_data[:, index] / 10)
-        if EXPLORE_UNUSED_DATA:
-            speeds = [7, 8]
-            plt.figure()
-            for i in speeds:
-                plt.plot(self.timestamps, self._full_data[:, i] / 10, label=str(i))
-            plt.gca().xaxis.set_major_formatter(self._time_format())
-            plt.legend()
-            plt.title('Speeds')
-            unused_indices = np.array([i for i in range(self._full_data.shape[1])
-                                       if not (i in mapping.keys() or i in speeds)])
-
-            plt.figure()
-            for i in unused_indices:
-                data = self._full_data[:, i] / 10
-                if np.all(np.isclose(data, 0)):
-                    continue
-                plt.plot(self.timestamps, data, label=str(i))
-            plt.gca().xaxis.set_major_formatter(self._time_format())
-            plt.legend()
+        self.timestamps = timestamps
+        self._data = data
+        for index, name in self.MAPPING.items():
+            setattr(self, name, self._data[:, index])
 
     @staticmethod
     def total_power(series: np.ndarray):
@@ -91,19 +60,62 @@ class Dataset:
         return self.heating / self.consumption
 
     @property
-    def daily_cop(self):
+    def mean_cop(self):
         return np.nanmean(self.cop)
+
+
+class DayData(Dataset):
+    def __init__(self, date: datetime.date):
+        self.date = date
+        use_cache = True
+        if use_cache:
+            try:
+                with open(self._cache_file) as file:
+                    contents = file.read()
+            except FileNotFoundError:
+                contents = self.get_data_from_server()
+                if self.date != datetime.datetime.today().date():
+                    # do not cache today's data as it will change
+                    with open(self._cache_file, 'w') as file:
+                        file.write(contents)
+        else:
+            contents = self.get_data_from_server()
+        timestamps, full_data = self.process_file_data(contents)
+        super().__init__(timestamps, full_data)
+
+        if EXPLORE_UNUSED_DATA:
+            self.explore_unused()
 
     def chunks(self):
         is_on = (self.consumption != 0).astype(int)
         switches = np.diff(is_on)
         starts = np.nonzero(switches == 1)[0]
         ends = np.nonzero(switches == -1)[0]
-        # line below will fail if on over midnight.
+        # line below will fail if the heat pump on over midnight.
         # If this occurs a special case will be needed
         assert starts.size == ends.size
         # returns inclusive inds (first non element and last nonzero element)
         return zip(starts+1, ends)
+
+    def explore_unused(self):
+        speeds = [7, 8]
+        plt.figure()
+        for i in speeds:
+            plt.plot(self.timestamps, self._data[:, i] / 10, label=str(i))
+        plt.gca().xaxis.set_major_formatter(TIME_FORMAT)
+        plt.legend()
+        plt.title('Speeds')
+        unused_indices = np.array([i for i in range(self._data.shape[1])
+                                   if not (i in self.MAPPING.keys() or i in speeds)])
+
+        plt.figure()
+        for i in unused_indices:
+            data = self._data[:, i] / 10
+            if np.all(np.isclose(data, 0)):
+                continue
+            plt.plot(self.timestamps, data, label=str(i))
+        plt.gca().xaxis.set_major_formatter(TIME_FORMAT)
+        plt.legend()
 
     def plot(self, axes=None):
         if axes is None:
@@ -112,26 +124,28 @@ class Dataset:
             ax2 = ax1.twinx()
         else:
             ax1, ax2 = axes
-        ax1.plot(self.timestamps, self.consumption, label=f'Electrical power: {self.total_consumption:.2f} kWh', color='red')
-        ax1.plot(self.timestamps, self.heating, label=f'Heating power: {self.total_heating:.2f} kWh', color='lightgreen')
+        ax1.plot(self.timestamps, self.consumption, label=f'Electrical power: {self.total_consumption:.2f} kWh',
+                 color='red')
+        ax1.plot(self.timestamps, self.heating, label=f'Heating power: {self.total_heating:.2f} kWh',
+                 color='lightgreen')
         ax1.set_ylabel('kW')
-        ax2.plot(self.timestamps, self.cop, label=f'Mean COP: {self.daily_cop:.2f}')
+        ax2.plot(self.timestamps, self.cop, label=f'Mean COP: {self.mean_cop:.2f}')
         ax1.legend(loc='upper right')
         ax2.legend(loc='upper left')
-        plt.suptitle(f'{self.date_str}')
-        ax1.xaxis.set_major_formatter(self._time_format())
+        # plt.suptitle(f'{self.date_str}')
+        ax1.xaxis.set_major_formatter(TIME_FORMAT)
 
         x_data = []
         y_data = []
         labels = []
         for chunk in self.chunks():
-            chunk_inds = range(chunk[0], chunk[1]+1)
+            chunk_inds = range(chunk[0], chunk[1] + 1)
             chunk_heating = self.heating[chunk_inds]
             chunk_consumption = self.consumption[chunk_inds]
             chunk_cop = self.cop[chunk_inds]
             assert not np.any(np.isnan(chunk_cop))
             x_data.append(self.timestamps[chunk[0]])
-            y_data.append(np.max(chunk_heating)+0.1)
+            y_data.append(np.max(chunk_heating) + 0.1)
             label = (f'{self.total_power(chunk_consumption):.1f} kWh\n'
                      + f'PF: {np.mean(chunk_cop):.2f}')
             labels.append(label)
@@ -152,20 +166,16 @@ class Dataset:
         plt.plot(self.timestamps, self.production_return, label='Production Return')
         plt.plot(self.timestamps, self.brine_supply, label='Brine Return')
         plt.plot(self.timestamps, self.brine_return, label='Brine Return')
-        plt.gca().xaxis.set_major_formatter(self._time_format())
+        plt.gca().xaxis.set_major_formatter(TIME_FORMAT)
         plt.legend()
 
         plt.figure()
         plt.plot(self.timestamps, self.outdoor_temp, label='Outdoor')
-        plt.gca().xaxis.set_major_formatter(self._time_format())
+        plt.gca().xaxis.set_major_formatter(TIME_FORMAT)
         plt.legend()
 
         plt.show()
         return [ax1, ax2]
-
-    @staticmethod
-    def _time_format():
-        return DateFormatter("%H:%M")
 
     @property
     def date_str(self):
@@ -198,7 +208,7 @@ class Dataset:
                 _, timestamp, *entry = line.split(';')[:-1]
                 timestamps.append(datetime.datetime.strptime(timestamp, CSV_DATE_FORMAT))
                 data.append([float(val) for val in entry])
-        data = np.array(data)
+        data = np.array(data) / 10
         return timestamps, data
 
 
@@ -238,9 +248,11 @@ def main():
     year = 2022
     month = 3
     # day = 10
+    datasets = []
     for day in range(20, 21):
-        data = Dataset(datetime.date(year, month, day))
+        data = DayData(datetime.date(year, month, day))
         data.plot()
+        datasets.append(data)
 
 
 if __name__ == '__main__':
