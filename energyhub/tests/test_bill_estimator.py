1808548f7f29b2eb921dfcebcaf462417f784ced
Ed Rogers <etfrogers@hotmail.com>
2023-04-08 16:16:08 +0100

    Add tests against values from PDF bills

--- energyhub/tests/test_bill_estimator.py
+++ energyhub/tests/test_bill_estimator.py
@@ -1,11 +1,9 @@
 import datetime
-import zoneinfo
 
 import numpy as np
 import pytest
 
 from ..bill_estimator import BillEstimator, MissingMeterReadingError
-from ..config import config
 
 
 test_days = [datetime.date(2022, 11, 25),
@@ -16,6 +14,16 @@ test_days = [datetime.date(2022, 11, 25),
              datetime.date(2023, 4, 2),
              datetime.date(2023, 4, 1),
              ]
+
+real_bills = [  # prices exc VAT
+    # date, total consumption, price (£)
+    (datetime.date(2022, 12, 2), 17.57, 5.73),
+    (datetime.date(2022, 12, 1), 76.53, 24.95),
+    (datetime.date(2022, 11, 30), 31.60, 10.30),
+    (datetime.date(2022, 11, 29), 25.20, 8.21),
+    (datetime.date(2022, 11, 28), 7.95, 2.56),
+    (datetime.date(2022, 11, 27), 52.02, 9.55),
+]
 
 
 @pytest.mark.parametrize('day', test_days)
@@ -58,3 +66,27 @@ def test_estimate_vs_calculation(day, inc_vat):
     bill_percent_error = 100 * bill_difference / octopus_bill
     print(f'Bill error £{bill_difference/100:.2f} ({bill_percent_error:.0f}%)')
     assert np.isclose(se_bill, octopus_bill, atol=2, rtol=0.03)
+
+
+@pytest.mark.parametrize('day, actual_consumption, actual_price', real_bills)
+def test_api_against_bills(day, actual_consumption, actual_price):
+    est = BillEstimator()
+    se_bill = est.estimate_bill_for_day(day, inc_vat=False)
+    api_consumption = est.get_consumption_for_day(day)
+    calculated_bill = est.calculate_bill_for_day(day, inc_vat=False)
+    # only matches dues to more than 0.01 due to different rounding?
+    assert np.isclose(sum(api_consumption), actual_consumption, atol=0.05)
+    # actual price in £, calculation in pence. Should match to the nearest penny, but
+    # allow 2p, due to rounding one value in one direction, one in the other
+    assert np.isclose(calculated_bill, actual_price*100, atol=2)
+
+
+@pytest.mark.parametrize('day, actual_consumption, actual_price', real_bills)
+def test_solaredge_estimate_against_bills(day, actual_consumption, actual_price):
+    est = BillEstimator()
+    estimated_consumption = est.estimate_consumption_from_solar_edge(day)
+    estimated_bill = est.estimate_bill_for_day(day, inc_vat=False)
+    # match to 0.1 kWh?
+    assert np.isclose(sum(estimated_consumption), actual_consumption, atol=0.1, rtol=0.05)
+    # actual price in £, calculation in pence. Should match to within 11p
+    assert np.isclose(estimated_bill, actual_price*100, atol=11)
