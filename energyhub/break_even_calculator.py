559df5e2076797e1388a7011514f4bf5ba14c97c
Ed Rogers <etfrogers@hotmail.com>
2023-04-21 05:08:42 +0100

    Update break-even calculator for new solar edge client

--- energyhub/break_even_calculator.py
+++ energyhub/break_even_calculator.py
@@ -6,38 +6,27 @@ from matplotlib.dates import DateFormatter
 
 from energyhub.config import config
 from solaredge import SolarEdgeClient
-from solaredge.solar_edge_api import API_TIME_FORMAT
 
 
 def main():
     se = SolarEdgeClient(config.data['solar-edge']['api-key'],
                          config.data['solar-edge']['site-id'])
-    details = se.get_energy_details(datetime(2022, 4, 1), datetime(2023, 3, 31), 'WEEK')
-    details = details['energyDetails']
-    assert details['timeUnit'] == 'WEEK'
-    meters = details['meters']
-    new_meters = {}
-    for meter in meters:
-        values = meter['values']
-        dates = [datetime.strptime(v['date'], API_TIME_FORMAT) for v in values]
-        energies = [v.get('value', np.nan) for v in values]
-        new_meters[meter['type']] = {'dates': np.array(dates),
-                                     'energies': np.array(energies)/1000,
-                                     }
-    for name, meter in new_meters.items():
-        plt.plot(meter['dates'], meter['energies'], label=name)
+    meters = se.get_energy_details(datetime(2022, 4, 1), datetime(2023, 3, 31), 'WEEK')
+    dates = meters.pop('timestamps')
+    for name, meter in meters.items():
+        plt.plot(dates, meter, label=name)
     plt.gca().xaxis.set_major_formatter(DateFormatter('%b'))
     plt.legend()
 
     plt.figure()
-    for name, meter in new_meters.items():
-        plt.plot(meter['dates'], np.cumsum(meter['energies']), label=name)
+    for name, meter in meters.items():
+        plt.plot(dates, np.cumsum(meter), label=name)
     plt.gca().xaxis.set_major_formatter(DateFormatter('%b'))
     plt.legend()
 
     plt.figure()
-    plt.plot(new_meters['Purchased']['dates'],
-             np.cumsum(new_meters['FeedIn']['energies']-new_meters['Purchased']['energies']))
+    plt.plot(dates,
+             np.cumsum(meters['FeedIn'] - meters['Purchased']))
     plt.gca().xaxis.set_major_formatter(DateFormatter('%m'))
     plt.show()
 
